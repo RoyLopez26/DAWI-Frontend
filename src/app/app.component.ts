92543296af67a6da113440fab963163c759af72c
Roy Lopez <lopezyanquiroy@gmail,com>
2024-04-03 10:02:27 -0500

    prueba de commit para DAWI - Fronted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {AppMaterialModule} from './app.material.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, AppMaterialModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
