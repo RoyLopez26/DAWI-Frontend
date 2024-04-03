09a585eaedd14eb89109a4948c318bc680fcd7f2
Roy Lopez <lopezyanquiroy@gmail,com>
2024-04-03 10:05:46 -0500

    prueba de commit

---
+++ src/app/components/add-modalidad/add-modalidad.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {AppMaterialModule} from '../../app.material.module';
+
+@Component({
+  selector: 'app-add-modalidad',
+  standalone: true,
+  imports: [AppMaterialModule],
+  templateUrl: './add-modalidad.component.html',
+  styleUrl: './add-modalidad.component.css'
+})
+export class AddModalidadComponent {
+
+}
